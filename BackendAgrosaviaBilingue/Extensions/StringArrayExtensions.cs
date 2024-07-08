92fb7b5ef3c5bc49023a4967e10cd7bf23c6709c
Daniel Bartels <sanbartels@gmail.com>
2024-07-08 17:23:27 -0500

    feact: Search core and template

---
+++ BackendAgrosaviaBilingue/Extensions/StringArrayExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Examine;
+using Examine.Search;
+using System.Collections.Generic;
+
+namespace BackendAgrosaviaBilingue.Extensions
+{
+    public static class StringArrayExtensions
+    {
+        public static IExamineValue[] Fuzzy(this string[] terms, float fuzzy)
+        {
+            if (terms == null) return null;
+
+            List<IExamineValue> values = new List<IExamineValue>();
+            foreach (var item in terms)
+            {
+                values.Add(item.Fuzzy(fuzzy));
+            }
+            return values.ToArray();
+        }
+
+        public static IExamineValue[] Boost(this string[] terms, float boost)
+        {
+            if (terms == null) return null;
+
+            List<IExamineValue> values = new List<IExamineValue>();
+            foreach (var item in terms)
+            {
+                values.Add(item.Boost(boost));
+            }
+            return values.ToArray();
+        }
+    }
+}
